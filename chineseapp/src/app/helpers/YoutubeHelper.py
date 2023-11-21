4d451a4b2c3932d98358555a0585be02d1f02e4d
noobcoda <yuemaykl@gmail.com>
2023-11-21 13:06:08 +0000

    submodule stuff

--- chineseapp/src/app/helpers/YoutubeHelper.py
+++ chineseapp/src/app/helpers/YoutubeHelper.py
@@ -7,6 +7,8 @@ import hanzidentifier
 import threading
 import requests
 from src.app.config import Config
+from transformers import AutoTokenizer, BertForTokenClassification
+from transformers import pipeline
 
 #     @article{guhr-EtAl:2021:fullstop,
     #   title={FullStop: Multilingual Deep Models for Punctuation Prediction},
@@ -44,13 +46,17 @@ class YouTubeHelper:
         self.pinyin_cache = {}
         self.bing_url = "https://api.bing.microsoft.com/v7.0/images/search"
         self.subscription_key = Config.BING_IMG_API_KEY
-
+        
+        self.TAG = "raynardj/classical-chinese-punctuation-guwen-biaodian"
+        self.model = BertForTokenClassification.from_pretrained(self.TAG)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.TAG)
+        self.ner = pipeline("ner",self.model,tokenizer=self.tokenizer)
 
     def init_helper(self):
         with self._init_lock:
             if self.stanza_nlp is None:
                 try:
-                    self.stanza_nlp = stanza.Pipeline('zh', download_method=DownloadMethod.REUSE_RESOURCES, use_gpu=False, verbose=False)
+                    self.stanza_nlp = stanza.Pipeline(lang='zh', processors='tokenize, lemma', download_method=DownloadMethod.REUSE_RESOURCES, use_gpu=False, verbose=False)
                 except Exception as e:
                     print("Error initializing stanza pipeline:", str(e))
 
@@ -83,6 +89,14 @@ class YouTubeHelper:
         print("finished simplified!")
         return simplified_transcript.strip()
     
+    
+    def generate_punctuation(self,x: str):
+        outputs = self.ner(x)
+        x_list = list(x)
+        for i, output in enumerate(outputs):
+            x_list.insert(output['end']+i, output['entity'])
+        return "".join(x_list)
+    
     def get_pinyin(self, word):
         if hanzidentifier.has_chinese(word):
             return pinyin.get(word)
@@ -98,9 +112,6 @@ class YouTubeHelper:
         if not self.stanza_nlp:
             self.init_helper()
 
-        # Split the text into chunks of approximately 1000 characters -- WHAT IF MID SENTENCE?!
-        text_chunks = [concatenated_text[i:i+1000] for i in range(0, len(concatenated_text), 1000)]
-
         sentence_and_words = []
         for text_chunk in text_chunks:
             try:
@@ -110,6 +121,7 @@ class YouTubeHelper:
                     seg_res = []
                     for word in sentence.words:
                         word_text = word.text.strip()
+                        word_upos = word.upos.strip()
                         calculated_pinyin = self.pinyin_cache.get(word_text)
                         if not calculated_pinyin:
                             calculated_pinyin = self.get_pinyin(word_text)
@@ -121,6 +133,7 @@ class YouTubeHelper:
                             self.translation_cache[word_text] = calculated_translation
                         entry = {
                             "word": word_text,
+                            "upos": word_upos,
                             "word_lemma": word.lemma.strip(),
                             "word_pos": word.pos.strip(), #IS PROPN/ NOUN/ AUX etc
                             "pinyin": calculated_pinyin,
